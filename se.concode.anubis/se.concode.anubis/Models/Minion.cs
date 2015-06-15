7e9d7c8edddd1a47595337243c7bcfca6858a844
Jovan Paskota <jovan@paskota.com>
2015-06-15 15:34:05 +0200

    REMOVED refit

---
+++ se.concode.anubis/se.concode.anubis/Models/Minion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace se.concode.anubis.Models
+{
+   public class Minion
+   {
+      public class Minion
+      {
+         public string Id { get; set; }
+         public string Name { get; set; }
+         public string Category { get; set; }
+         public string ExtIp { get; set; }
+         public string IntIp { get; set; }
+         public string Description { get; set; }
+         public string Note { get; set; }
+         public string Login { get; set; }
+         public string Pass { get; set; }
+         public int? Disksize { get; set; }
+         public int? Disksizefree { get; set; }
+      }
+   }
+}
